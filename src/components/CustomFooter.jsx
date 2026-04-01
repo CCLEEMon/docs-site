0a83ab6666054fb015c6312e31ab669da93d617b
CCLEEMon <leecc1531@gmail.com>
2026-04-02 00:41:05 +0800

    feat: 英文站页脚添加 X 和 LinkedIn 社媒图标

--- src/components/CustomFooter.jsx
+++ src/components/CustomFooter.jsx
@@ -77,18 +77,43 @@ export default function CustomFooter() {
               )}
 
               {/* Upwork - 仅英文站 */}
+              {/* 英文站社媒 */}
               {isEn && (
-                <a
-                  href="https://www.upwork.com/freelancers/~010ab5ec29d8f4ff3f"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-gray-500 dark:text-gray-400 hover:text-white dark:hover:text-gray-200 transition-colors duration-200"
-                  aria-label="Upwork"
-                >
-                  <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M18.561 13.158c-1.698 0-3.074-1.376-3.074-3.074s1.376-3.074 3.074-3.074 3.074 1.376 3.074 3.074-1.376 3.074-3.074 3.074zm-6.478-6.479h-2.81v8.372c0 .838-.68 1.518-1.518 1.518s-1.518-.68-1.518-1.518V6.68H5.427v8.372c0 2.392 1.944 4.336 4.336 4.336s4.336-1.944 4.336-4.336V6.68h-.016zm12.892-2.81H22.17v10.645c0 .838-.68 1.518-1.518 1.518-.468 0-.887-.212-1.163-.546 1.023-.973 1.662-2.345 1.662-3.867 0-2.961-2.409-5.37-5.37-5.37s-5.37 2.409-5.37 5.37 2.409 5.37 5.37 5.37c.996 0 1.928-.274 2.732-.747.766 1.027 1.98 1.695 3.35 1.695 2.32 0 4.207-1.887 4.207-4.207V3.868h-.078z"/>
-                  </svg>
-                </a>
+                <>
+                  <a
+                    href="https://www.upwork.com/freelancers/~010ab5ec29d8f4ff3f"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-500 dark:text-gray-400 hover:text-white dark:hover:text-gray-200 transition-colors duration-200"
+                    aria-label="Upwork"
+                  >
+                    <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M18.561 13.158c-1.698 0-3.074-1.376-3.074-3.074s1.376-3.074 3.074-3.074 3.074 1.376 3.074 3.074-1.376 3.074-3.074 3.074zm-6.478-6.479h-2.81v8.372c0 .838-.68 1.518-1.518 1.518s-1.518-.68-1.518-1.518V6.68H5.427v8.372c0 2.392 1.944 4.336 4.336 4.336s4.336-1.944 4.336-4.336V6.68h-.016zm12.892-2.81H22.17v10.645c0 .838-.68 1.518-1.518 1.518-.468 0-.887-.212-1.163-.546 1.023-.973 1.662-2.345 1.662-3.867 0-2.961-2.409-5.37-5.37-5.37s-5.37 2.409-5.37 5.37 2.409 5.37 5.37 5.37c.996 0 1.928-.274 2.732-.747.766 1.027 1.98 1.695 3.35 1.695 2.32 0 4.207-1.887 4.207-4.207V3.868h-.078z"/>
+                    </svg>
+                  </a>
+                  <a
+                    href="https://x.com/CaichenLee"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-500 dark:text-gray-400 hover:text-white dark:hover:text-gray-200 transition-colors duration-200"
+                    aria-label="X"
+                  >
+                    <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+                    </svg>
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/%E6%8C%AF%E6%8C%AF-%E6%9D%8E-9b0b113bb/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-500 dark:text-gray-400 hover:text-white dark:hover:text-gray-200 transition-colors duration-200"
+                    aria-label="LinkedIn"
+                  >
+                    <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+                    </svg>
+                  </a>
+                </>
               )}
             </div>
           </div>
