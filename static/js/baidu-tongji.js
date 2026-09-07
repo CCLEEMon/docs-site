8f95120a67b0683f2031d747cc728da5dc95de08
cclee-hub <leecc1531@gmail.com>
2026-09-07 08:36:22 +0800

    chore(zh): 百度统计站点 ID 更新为 2a1172ee

--- static/js/baidu-tongji.js
+++ static/js/baidu-tongji.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function () {
   var hm = document.createElement('script');
-  hm.src = 'https://hm.baidu.com/hm.js?09226d95abb7d39ad62158b28bc338c5';
+  hm.src = 'https://hm.baidu.com/hm.js?2a1172ee33e0da6a8590940b410fd56e';
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(hm, s);
 })();
